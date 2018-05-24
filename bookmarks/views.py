7a9a2112c8b983964e230ed331b3dfef9f594d04
umonika <39208561+umonika@users.noreply.github.com>
2018-05-24 08:59:30 +0530

    add file

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-    return HttpResponse ('welcome to Django bookmarks')
+    output = '''
+    <html>
+    <head><title> %s </title></head>
+    <body>
+    <h1>%s</h1>
+    <p> %s</p>
+    </body>
+    </html>''' % (
+        'Django Bookmarks',
+        'welcome to Django Bookmarks',
+        'where you can store and share bookmarks')
+
+    return HttpResponse (output)
+
+
