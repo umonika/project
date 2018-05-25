7fee9fb3a6d8272eb202540505db853a256d92e0
umonika <39208561+umonika@users.noreply.github.com>
2018-05-25 08:39:37 +0530

    changes

--- bookmarks_app/settings.py
+++ bookmarks_app/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bookmarks_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR),'Bookmarks','templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
