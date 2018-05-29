1067806fdef27b6708e7d968a6a2282ff9b0d5c4
umonika <39208561+umonika@users.noreply.github.com>
2018-05-29 19:40:59 +0530

    add file

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import main_page
+from .views import main_page,user_page
 urlpatterns = [
     path('', main_page),
+    path('user/<str:username>/',user_page)
 ]
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import HttpResponse
+
+from django.contrib.auth.models import User
+
+from django.http import Http404, HttpResponse
+
 from django.shortcuts import render
+
 from django.template import loader
-# Create your views here.
+
+
+
+
+
 def main_page(request):
+
     template = loader.get_template('bookmarks/main_page.html')
-    return HttpResponse(template.render({} , request))
+    return HttpResponse(template.render({}, request))
 
 
- 
+
+
+
+def user_page(request, username):
+
+    try:
+
+        user = User.objects.get(username=username)
+
+    except Exception:
+
+        raise Http404('Requested user not found')
+
+    bookmarks = user.bookmark_set.all()
+
+    template = loader.get_template('bookmarks/user_page.html')
+
+    context = {
+
+        'username': username,
+
+        'bookmarks': bookmarks
+
+    }
+
+    return HttpResponse(template.render(context, request))
