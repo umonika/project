9e9c3f3028d45ca4f4553ff39039144dee776c8e
umonika <39208561+umonika@users.noreply.github.com>
2018-05-29 08:32:44 +0530

    add files

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -11,3 +11,7 @@ class Bookmark(models.Model):
     title = models.CharField( max_length = 200)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     link = models.ForeignKey(Link, on_delete = models.CASCADE)
+    def __str__(self):
+        return '''title: {0},user: {1},link: {2}''' .format(self.title,
+                                                    self.user,
+                                                    self.link )   
